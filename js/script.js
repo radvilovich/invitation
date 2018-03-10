4e86425f67468671da391b6cafc0d542e3493a9d
alex.rozhnov <alex.rozhnov@capsilon.com>
2018-03-10 18:28:59 +0300

    added script settings for birthday date

--- js/script.js
+++ js/script.js
@@ -470,12 +470,12 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2017/12/25', function(event) {
+        $('#clock').countdown('2018/03/23', function(event) {
             var $this = $(this).html(event.strftime(''
-            + '<div class="box"><div>%D</div> <span>Days</span> </div>'
-            + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
-            + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
-            + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
+            + '<div class="box"><div>%D</div> <span>Дней</span> </div>'
+            + '<div class="box"><div>%H</div> <span>Часов</span> </div>'
+            + '<div class="box"><div>%M</div> <span>Минут</span> </div>'
+            + '<div class="box"><div>%S</div> <span>Секунд</span> </div>'));
         });
     }
 
